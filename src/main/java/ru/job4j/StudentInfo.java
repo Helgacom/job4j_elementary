1b50f3662552071710342588ef2d8c14afb9d5de
Olga Remneva <remneva_olga@mail.ru>
2022-04-21 12:18:46 +0400

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Olga Remneva");
+        System.out.println("02.11.1985");
+    }
+}
